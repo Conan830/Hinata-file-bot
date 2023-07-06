1f5501ce08ac975304c0f4b358d14cdc5b17edc3
Trippy <138500712+Trippy07@users.noreply.github.com>
2023-07-06 23:42:01 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "About":
+    if data == "about":
         await query.message.edit_text(
             text = f"<b>○ Creator : <a href='tg://user?id={5493260414}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @HANIME_NATION\n○ Support Group : @ANIMETALKS0</b>",
             disable_web_page_preview = True,
