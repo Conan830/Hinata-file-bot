e7de037d4cb7f570ec98986f0acaf84445bfed8c
Detective Conan <109863157+Conan830@users.noreply.github.com>
2023-09-05 08:59:33 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={5493260414}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @Hanime_Xyz\n○ Support Group : @Animetalks0</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id=1956698956'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @toonsweboffical Support Group : @Toonsweb_Disscussion_Group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
